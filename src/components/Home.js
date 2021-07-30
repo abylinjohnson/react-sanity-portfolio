b0ef9f0bb4029601c9a9abb3be0017f9b5056eb4
Abylin Johnson <jgabylin@karunya.edu.in>
2021-07-31 00:53:29 +0530

    completed the Portfolio Website

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,18 @@
 import React from "react";
-
+import image from "../assets/bg.jpg";
 export default function Home() {
-  return <h1>homePage</h1>;
+  return (
+    <main>
+      <img
+        src={image}
+        className="absolute object-cover w-full h-full"
+        alt="Background"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Welcome To Crooked Codes!
+        </h1>
+      </section>
+    </main>
+  );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,67 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import SanityClient from "../client";
 export default function Project() {
-  return <h1>Project Page</h1>;
+  const [projectData, setProjectData] = useState(null);
+  useEffect(() => {
+    SanityClient.fetch(
+      `*[_type == "project"]{
+      title,
+      date,
+      place,
+      description,
+      projctType,
+      link,
+      tag
+    }`
+    )
+      .then((data) => setProjectData(data))
+      .catch(console.error);
+  }, []);
+  return (
+    <main className="bg-green-200 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex justify-center cursive">My Pojects</h1>
+        <h2 className="text-lg text-gray-600 flex justify-center mb-12">
+          Welcome To my project page!
+        </h2>
+        <section className="grid grid-cols-2 gap-8">
+          {projectData &&
+            projectData.map((project, index) => (
+              <article className="relative rounded-lg shadow-xl bg-white p-16">
+                <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
+                  <a
+                    href={project.link}
+                    alt={project.title}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {project.title}
+                  </a>
+                </h3>
+                <div className="text-gray-500 text-xs space-x-4">
+                  <span>
+                    <strong className="font-bold">Finished on</strong>:{" "}
+                    {new Date(project.date).toLocaleDateString()}
+                  </span>
+                  <p className="my-6 text-lg text-gray-700 leading-relaxed">
+                    {project.description}
+                  </p>
+                  <a
+                    href={project.link}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="text-red-500 font-bold hover:underline hover:text-red"
+                  >
+                    View The Project{" "}
+                    <span role="img" aria-label="right pointer">
+                      👉
+                    </span>
+                  </a>
+                </div>
+              </article>
+            ))}
+        </section>
+      </section>
+    </main>
+  );
 }
